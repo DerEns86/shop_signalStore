091bff25e38c404b58806065bb3f4143c2e206f1
DerEns86 <v.ens86@yahoo.de>
2025-05-16 17:08:33 +0200

    update header component with logout method

--- src/app/shop/components/shared/header/header.component.ts
+++ src/app/shop/components/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { UserStore } from '../../../../auth.store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,12 @@ import { UserStore } from '../../../../auth.store';
 })
 export class HeaderComponent {
   readonly userStore = inject(UserStore);
+  readonly router = inject(Router);
+
+  handleLogout() {
+    this.userStore.logout();
+    console.log(this.userStore.getUser());
+
+    this.router.navigateByUrl('/');
+  }
 }
