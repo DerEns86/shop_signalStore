89dfeabe97525d6dac132a115241e28b30ca91b0
DerEns86 <v.ens86@yahoo.de>
2025-05-20 14:36:23 +0200

    add ProductApiService with CRUD methods

---
+++ src/app/shop/services/product-api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductApiService {
+  private http: HttpClient = inject(HttpClient);
+
+  private readonly apiUrl = 'https://fakestoreapi.com/products';
+
+  fetchProducts() {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  addProduct(product: Product) {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  updateProduct(product: Product) {
+    return this.http.put<Product>(`${this.apiUrl}/${product.id}`, product);
+  }
+
+  deleteProduct(productId: number) {
+    return this.http.delete<Product>(`${this.apiUrl}/${productId}`);
+  }
+}
